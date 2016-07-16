97a120ced6a5a40b59d180e5c2d5814190f7eaaa
Danis Ziganshin <zigdanis@gmail.com>
2016-07-16 19:42:48 +0300

    Setup project

---
+++ InteractiveTransitioning/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  InteractiveTransitioning
+//
+//  Created by Danis Ziganshin on 15/07/16.
+//  Copyright © 2016 Zigdanis. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ InteractiveTransitioning/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InteractiveTransitioning
+//
+//  Created by Danis Ziganshin on 15/07/16.
+//  Copyright © 2016 Zigdanis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupGestureRecognizers()
+    }
+    
+    func setupGestureRecognizers() {
+        
+    }
+
+}
+
